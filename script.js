69d71ae2dc1690e90f909c7c8c5cccb437097142
Oybek <o.kuranboyev@gmail.com>
2022-02-23 21:33:41 +0500

    js1dars

---
+++ script.js
@@ -0,0 +1,82 @@
+var ism = prompt('Ismizgizni yozing')
+var yosh = prompt("Yoshingizni yozing")
+var misolbirinchi = " Misolni yeching "
+console.log("Mening ismim " + ism)
+console.log("Mening yoshim " + yosh + "da")
+
+
+var num1 = " 2+3=? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 2
+var b = 3
+console.log("Misol 1:" + (" 2 + 3 = " + (a + b)) + " Sizning javob " + birinchi)
+
+var num1 = " 201+301=? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 201
+var b = 301
+console.log("Misol 1:" + (" 201 + 301 = " + (a + b)) + " Sizning javob " + birinchi)
+
+
+
+var num1 = " 8-3=? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 8
+var b = 3
+console.log("Misol 2:" + (" 8 - 3 = " + (a - b)) + " Sizning javob " + birinchi)
+
+var num1 = " 119-139=? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 119
+var b = 139
+console.log("Misol 2.1:" + (" 119 - 139 = " + (a - b)) + " Sizning javob " + birinchi)
+
+
+
+
+var num1 = " 3 * 5 =? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 3
+var b = 5
+console.log("Misol 3:" + (" 3 * 5 = " + (a * b)) + " Sizning javob " + birinchi)
+
+var num1 = " 4 * 8 =? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 4
+var b = 8
+console.log("Misol 3.1:" + (" 4 * 8 = " + (a * b)) + " Sizning javob " + birinchi)
+
+
+
+var num1 = " 120 / 5 =? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 120
+var b = 5
+console.log("Misol 4:" + (" 120 / 5 = " + (a / b)) + " Sizning javob " + birinchi)
+
+var num1 = " 189 / 3 =? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 189
+var b = 3
+console.log("Misol 4.1:" + (" 189 / 3 = " + (a / b)) + " Sizning javob " + birinchi)
+
+
+var num1 = " 120 % 5 =? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 120
+var b = 5
+console.log("Misol 5:" + (" 120 % 5 = " + (a % b)) + " Sizning javob " + birinchi)
+
+
+
+var num1 = " 83 % 5 =? "
+var birinchi = prompt(misolbirinchi + num1)
+var a = 83
+var b = 5
+console.log("Misol 5.1:" + (" 83 % 5 = " + (a % b)) + " Sizning javob " + birinchi)
+
+
+
+
+
+alert("Javobni bilish uchun konsolga o'ting")
